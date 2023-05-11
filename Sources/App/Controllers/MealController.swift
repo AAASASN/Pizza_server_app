239dd75e40f9179c4d436d6e49e8f60d2c6cdf93
Александр Мараенко <alexander@MacBook-Air-Aleksandr.local>
2023-05-12 02:02:04 +0300

    MealController.swift changed, methods createHandlerAll and deleteHandlerAll added

--- Sources/App/Controllers/MealController.swift
+++ Sources/App/Controllers/MealController.swift
@@ -13,20 +13,36 @@ struct MealController: RouteCollection {
         let mealsRoutes = routes.grouped("api", "meals")
         
         mealsRoutes.get(use: getAllHandler)
-        mealsRoutes.get (":mealID", use: getHandler)
-        mealsRoutes.post(use: createHandler)
-        mealsRoutes.put (":mealID", use: updateHandler)
+        mealsRoutes.get(":mealID", use: getHandler)
+//        mealsRoutes.post(use: createHandler)
+        mealsRoutes.post(use: createHandlerAll)
+        mealsRoutes.put(":mealID", use: updateHandler)
         mealsRoutes.delete(":mealID", use: deleteHandler)
+        mealsRoutes.delete(use: deleteHandlerAll)
     }
     
     //MARK: CRUD - Create (Добавление в базу данных одного обеда)
-    func createHandler (_ req: Request) async throws -> Meal {
+    func createHandler(_ req: Request) async throws -> Meal {
         guard let meal = try? req.content.decode(Meal.self) else {
             throw Abort(.custom(code: 499, reasonPhrase: "Не получилось декодировать контент в модель Meal"))
         }
         try await meal.save(on: req.db)
         return meal
     }
+    
+    //MARK: CRUD - Create (Добавление в базу массива обедов)
+    func createHandlerAll(_ req: Request) async throws -> [Meal] {
+        
+        guard let meals = try? req.content.decode([Meal].self) else {
+            throw Abort(.custom(code: 499, reasonPhrase: "Не получилось декодировать контент в модель [Meal]"))
+        }
+        for meal in meals {
+            try await meal.save(on: req.db)
+        }
+        return meals
+    }
+    
+    
     
     //MARK: CRUD - Retrieve (Получение всех данных из базы)
     func getAllHandler(_ req: Request) async throws -> [Meal] {
@@ -43,7 +59,7 @@ struct MealController: RouteCollection {
 
 
 
-    // CRUD - Update (изменение одного обеда по ID)
+    // MARK: CRUD - Update (изменение одного обеда по ID)
     func updateHandler(_ req: Request) async throws -> Meal {
 
         let updatedMeal = try req.content.decode(Meal.self)
@@ -65,7 +81,7 @@ struct MealController: RouteCollection {
         return meal
     }
 
-    // CRUD - Delete (удаление одного обеда по ID)
+    // MARK: CRUD - Delete (удаление одного обеда по ID)
     func deleteHandler(_ req: Request) async throws ->  HTTPStatus {
         guard let meal = try await Meal.find(req.parameters.get ("mealID"), on: req.db) else {
             throw Abort(.notFound)
@@ -73,5 +89,21 @@ struct MealController: RouteCollection {
         try await meal.delete(on: req.db)
         return .noContent
 
+    }
+    
+    // MARK: CRUD - Delete (удаление всех обедов полностью)
+    func deleteHandlerAll(_ req: Request) async throws ->  HTTPStatus {
+        
+        guard let allMeal = try? await Meal.query(on: req.db).all()  else {
+            throw Abort(.notFound)
+        }
+        
+        for meal in allMeal {
+            try await meal.delete(on: req.db)
+            print("delete ok")
+        }
+        
+        return .noContent
+        
     }
 }
